b1a11aa61be99c415bb1c1f149ceb6054e0cb719
Andie.Phan <andie.phan@analog.com>
2023-09-20 10:03:22 -0700

    add path option for S3Store

--- s3dol/store.py
+++ s3dol/store.py
@@ -5,11 +5,12 @@ from s3dol.base import S3BucketDol, S3ClientDol
 from s3dol.utility import S3DolException
 
 
-def S3Store(bucket_name: str, *, make_bucket=False, **kwargs) -> Store:
+def S3Store(bucket_name: str, *, make_bucket=False, path=None, **kwargs) -> Store:
     """S3 Bucket Store
 
     :param bucket_name: name of bucket to store data in
     :param make_bucket: create bucket if it does not exist
+    :param path: prefix to use for bucket keys
     :param aws_access_key_id: AWS access key ID
     :param aws_secret_access_key: AWS secret access key
     :param aws_session_token: AWS session token
@@ -22,7 +23,7 @@ def S3Store(bucket_name: str, *, make_bucket=False, **kwargs) -> Store:
     validate_kwargs(kwargs)
     s3cr = S3ClientDol(**kwargs)
     validate_bucket(bucket_name, s3cr, make_bucket)
-    return S3BucketDol(client=s3cr.client, bucket_name=bucket_name)
+    return S3BucketDol(client=s3cr.client, bucket_name=bucket_name, prefix=path)
 
 
 def validate_kwargs(kwargs):
