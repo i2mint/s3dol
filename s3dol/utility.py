d010dce69e244755ba0ed5311914a0e925d14e2d
valentin-feron <val.frn@gmail.com>
2023-10-24 19:37:39 +0200

    fix pylint errors

--- s3dol/utility.py
+++ s3dol/utility.py
@@ -1,3 +1,30 @@
+"""
+This module provides utility functions and custom exceptions for the `s3dol` package.
+
+Classes:
+- S3DolException: Base exception class for `s3dol` package.
+- S3KeyError: Exception raised when a key error occurs.
+- NoSuchKeyError: Exception raised when a key is not found.
+- KeyNotValidError: Exception raised when a key is not valid.
+- GetItemForKeyError: Exception raised when an error occurs while getting an item for a key.
+- S3HttpError: Exception raised when an HTTP error occurs.
+- S3Not200StatusCodeError: Exception raised when an HTTP status code other than 200 is returned.
+
+Functions:
+- raise_on_error: Function that raises an error.
+- return_none_on_error: Function that returns None.
+- return_empty_tuple_on_error: Function that returns an empty tuple.
+- Resp.status_code: Function that returns the HTTP status code from a dictionary.
+- Resp.contents: Function that returns the contents from a dictionary.
+- Resp.key: Function that returns the key from a dictionary.
+- Resp.common_prefixes: Function that returns the common prefixes from a dictionary.
+- Resp.prefix: Function that returns the prefix from a dictionary.
+- Resp.buckets: Function that returns the buckets from a dictionary.
+- Resp.body: Function that returns the body from a dictionary.
+- Resp.ascertain_status_code: Function that raises an error if the HTTP status code is not 200.
+- Resp.ascertain_200_status_code: Function that raises an error if the HTTP status code is not 200 and the response is not a dictionary with a 'ResponseMetadata' key.
+"""
+""""""
 from dol.paths import path_get, OnErrorType
 
 
